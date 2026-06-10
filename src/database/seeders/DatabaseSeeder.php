1324866ae930a70f705895e34e5d0a5f9cdefb70
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-06-10 13:06:21 +0330

    (feat): edit DatabaseSeeder.php

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -5,9 +5,12 @@ namespace Database\Seeders;
 use App\Models\Post;
 use App\Models\PostView;
 use App\Models\User;
+use App\Services\PostViewService;
+use Carbon\CarbonPeriod;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,40 +21,64 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $users = User::factory(50)->create();
+        Schema::disableForeignKeyConstraints();
+        PostView::truncate();
+        Post::truncate();
+        User::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        User::factory()->create([
+            'email' => 'test@test.com',
+            'password' => '123@QWER'
+        ]);
+
+        $users = User::factory(10)->create();
 
         $posts = collect();
 
         foreach ($users as $user) {
             $posts = $posts->merge(
                 Post::factory()
-                    ->count(rand(1, 10))
+                    ->count(rand(1, 5))
                     ->create([
                         'user_id' => $user->id,
                     ])
             );
         }
 
-        foreach ($posts as $post) {
+        $from = Carbon::now()->subMonths(2);
+        $to = Carbon::now();
 
-            $viewCount = rand(10, 500);
+        $period = CarbonPeriod::create($from, $to);
 
-            for ($i = 0; $i < $viewCount; $i++) {
+        dump('seeding started');
 
-                $viewer = rand(0, 100) < 70
-                    ? $users->random()
-                    : null;
+        foreach ($period as $range) {
+            foreach ($posts as $post) {
 
-                PostView::create([
-                    'post_id' => $post->id,
-                    'user_id' => $viewer?->id,
-                    'ip_address' => fake()->ipv4(),
-                    'user_agent' => fake()->userAgent(),
-                    'viewed_at' => Carbon::now()
-                        ->subDays(rand(0, 30))
-                        ->subMinutes(rand(0, 1440)),
-                ]);
+                foreach ($users as $user) {
+                    $viewer = rand(0, 100) < 50
+                        ? $user
+                        : null;
+
+                    $randNumDate = rand(10, 100) < 70;
+                    $randNumPost = rand(10, 100) < 70;
+                    if (!$randNumDate || $randNumPost) {
+                        continue;
+                    }
+
+
+                    app(PostViewService::class)->create([
+                        'post_id' => $post->id,
+                        'user_id' => $viewer?->id,
+                        'ip_address' => fake()->ipv4(),
+                        'user_agent' => fake()->userAgent(),
+                        'viewed_at' => $range
+                    ]);
+                }
             }
         }
+        dump('seeding finished');
+
     }
 }
